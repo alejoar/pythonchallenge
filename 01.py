a315bf23faa765f46a28e819d74476110c181c85
Alejo Arias <aarias@dotdash.com>
2019-02-18 11:49:59 +0100

    0, 1, 2

---
+++ 01.py
@@ -0,0 +1,5 @@
+# coding: utf-8
+c = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."
+alphabet = 'abcdefghijklmnopqrstuvwxyzab'
+print(''.join([alphabet[alphabet.find(item)+2] for item in c]).replace('b', ' '))
+print(''.join([alphabet[alphabet.find(item)+2] for item in 'map']).replace('b', ' '))
